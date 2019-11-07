4adcb00f10fa4f4d00049f885b4a3ff0c8318042
fikrimaualana <55828126+Fikrimaulana15@users.noreply.github.com>
2019-11-07 22:59:35 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118074/latihan58/tambahkurang/JumlahBilangan.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118074.latihan58.tambahkurang;
+
+/**
+ *
+ * @author ASUS
+ */
+public class JumlahBilangan extends Bilangan{
+    
+public void tampilHasilJumlah() {
+        System.out.println("Hasil perjumlahan = " + (getX() + getY()));
+    }
+}
---
+++ src/pbo2/pkg10118074/latihan58/tambahkurang/PBO210118074LATIHAN58TambahKurang.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118074.latihan58.tambahkurang;
+
+/**
+ *
+ * @author ASUS
+ * NAMA : FIKRI MAULANA
+ * KELAS :IF2
+ * NIM : 10118074
+ */
+public class PBO210118074LATIHAN58TambahKurang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    JumlahBilangan jumlahBilangan = new JumlahBilangan();
+        jumlahBilangan.tampilHasilJumlah();
+        
+        SelisihBilangan selisihBilangan = new SelisihBilangan();
+        selisihBilangan.tampilSelisih();
+        
+        System.out.println("developed by : fikri maulana ");
+    }
+    
+}
+    
+
